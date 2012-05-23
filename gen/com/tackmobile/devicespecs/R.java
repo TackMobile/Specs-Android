f3d8ce39faa6f2f4dbe20e264ecbd9778a49cfb1
Jason Vigil <vigiljt@gmail.com>
2012-05-23 12:33:12 -0600

    gave share button pressed/focused states

--- gen/com/tackmobile/devicespecs/R.java
+++ gen/com/tackmobile/devicespecs/R.java
@@ -16,7 +16,9 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int share=0x7f020001;
-        public static final int tack_logo=0x7f020002;
+        public static final int share_active=0x7f020002;
+        public static final int share_btn=0x7f020003;
+        public static final int tack_logo=0x7f020004;
     }
     public static final class id {
         public static final int action_bar=0x7f070000;
