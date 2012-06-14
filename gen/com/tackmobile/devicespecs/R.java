21dab22b0e89f0fca52f546a637ff8ee76643e0f
Joshua Jamison <joshua.jamison@gmail.com>
2012-06-14 09:05:37 -0600

    added app icon and adjusted header colors

--- gen/com/tackmobile/devicespecs/R.java
+++ gen/com/tackmobile/devicespecs/R.java
@@ -11,66 +11,71 @@ public final class R {
     public static final class array {
         /**  16+ ??? 
          */
-        public static final int version_code=0x7f050000;
+        public static final int version_code=0x7f060000;
     }
     public static final class attr {
     }
+    public static final class color {
+        public static final int grey=0x7f040001;
+        public static final int tack_blue=0x7f040000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int share=0x7f020001;
-        public static final int share_active=0x7f020002;
-        public static final int share_btn=0x7f020003;
-        public static final int tack_logo=0x7f020004;
+        public static final int icon=0x7f020001;
+        public static final int share=0x7f020002;
+        public static final int share_active=0x7f020003;
+        public static final int share_btn=0x7f020004;
+        public static final int tack_logo=0x7f020005;
     }
     public static final class id {
-        public static final int action_bar=0x7f070000;
-        public static final int codename_text=0x7f070004;
-        public static final int dpi_qual_text=0x7f070007;
-        public static final int dpi_text=0x7f070008;
-        public static final int dx_text=0x7f07000b;
-        public static final int fontscale_text=0x7f07000c;
-        public static final int locale_text=0x7f070005;
-        public static final int release_text=0x7f070003;
-        public static final int sdk_ver_text=0x7f070002;
-        public static final int size_text=0x7f070006;
-        public static final int spec_list=0x7f070001;
-        public static final int width_height_dp_text=0x7f07000a;
-        public static final int width_height_px_text=0x7f070009;
+        public static final int action_bar=0x7f080000;
+        public static final int codename_text=0x7f080004;
+        public static final int dpi_qual_text=0x7f080007;
+        public static final int dpi_text=0x7f080008;
+        public static final int dx_text=0x7f08000b;
+        public static final int fontscale_text=0x7f08000c;
+        public static final int locale_text=0x7f080005;
+        public static final int release_text=0x7f080003;
+        public static final int sdk_ver_text=0x7f080002;
+        public static final int size_text=0x7f080006;
+        public static final int spec_list=0x7f080001;
+        public static final int width_height_dp_text=0x7f08000a;
+        public static final int width_height_px_text=0x7f080009;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int codename=0x7f040016;
-        public static final int density_multiplier=0x7f040014;
-        public static final int dpi=0x7f040012;
-        public static final int font_scale=0x7f040017;
-        public static final int hdpi=0x7f04000e;
-        public static final int large=0x7f040009;
-        public static final int ldpi=0x7f04000c;
-        public static final int locale=0x7f040004;
-        public static final int mdpi=0x7f04000d;
-        public static final int normal=0x7f040008;
-        public static final int operating_system=0x7f040001;
-        public static final int other=0x7f040015;
-        public static final int release=0x7f040003;
-        public static final int resoultion=0x7f04000b;
-        public static final int screen=0x7f040005;
-        public static final int sdk_version=0x7f040002;
-        public static final int size=0x7f040006;
-        public static final int small=0x7f040007;
-        public static final int tvdpi=0x7f040010;
-        public static final int undefined=0x7f04000a;
-        public static final int unknown_dpi=0x7f040011;
-        public static final int width_height=0x7f040013;
-        public static final int xhdpi=0x7f04000f;
+        public static final int app_name=0x7f050000;
+        public static final int codename=0x7f050016;
+        public static final int density_multiplier=0x7f050014;
+        public static final int dpi=0x7f050012;
+        public static final int font_scale=0x7f050017;
+        public static final int hdpi=0x7f05000e;
+        public static final int large=0x7f050009;
+        public static final int ldpi=0x7f05000c;
+        public static final int locale=0x7f050004;
+        public static final int mdpi=0x7f05000d;
+        public static final int normal=0x7f050008;
+        public static final int operating_system=0x7f050001;
+        public static final int other=0x7f050015;
+        public static final int release=0x7f050003;
+        public static final int resoultion=0x7f05000b;
+        public static final int screen=0x7f050005;
+        public static final int sdk_version=0x7f050002;
+        public static final int size=0x7f050006;
+        public static final int small=0x7f050007;
+        public static final int tvdpi=0x7f050010;
+        public static final int undefined=0x7f05000a;
+        public static final int unknown_dpi=0x7f050011;
+        public static final int width_height=0x7f050013;
+        public static final int xhdpi=0x7f05000f;
     }
     public static final class style {
-        public static final int ListHeader=0x7f060001;
-        public static final int ListItem=0x7f060003;
-        public static final int ListRow=0x7f060000;
-        public static final int ListSeparator=0x7f060002;
-        public static final int ListValue=0x7f060004;
+        public static final int ListHeader=0x7f070001;
+        public static final int ListItem=0x7f070003;
+        public static final int ListRow=0x7f070000;
+        public static final int ListSeparator=0x7f070002;
+        public static final int ListValue=0x7f070004;
     }
 }
