0157edd6940ed0ef97cc206e903579ddbe5f584e
Anatoliy Adamitskiy <anatoliy.adamitskiy@tackmobile.com>
2016-04-25 11:44:34 -0600

    Updates to latest Android version as well as UI Updates

--- app/src/main/java/com/tackmobile/specs/SpecsActivity.java
+++ app/src/main/java/com/tackmobile/specs/SpecsActivity.java
@@ -6,25 +6,85 @@ import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TableLayout;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabClickListener;
 
 import java.util.ArrayList;
 
-public class SpecsActivity extends Activity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class SpecsActivity extends AppCompatActivity {
+  private ArrayList<Stat> deviceStats;
   private ArrayList<Stat> osStats;
   private ArrayList<Stat> screenStats;
   private ArrayList<Stat> otherStats;
+
+  private BottomBar mBottomBar;
+
+  @Bind(R.id.toolbar) Toolbar mToolbar;
+  @Bind(R.id.toolbarTitle) TextView mToolbarTitle;
+  @Bind(R.id.device_layout) TableLayout mDeviceLayout;
+  @Bind(R.id.os_layout) TableLayout mOsLayout;
+  @Bind(R.id.screen_layout) TableLayout mScreenLayout;
+  @Bind(R.id.other_layout) TableLayout mOtherLayout;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
+    ButterKnife.bind(this);
+    mToolbarTitle.setText(getDeviceName());
+    setSupportActionBar(mToolbar);
+    getSupportActionBar().setTitle("");
+    createBottomBar(savedInstanceState);
     loadStats();
   }
 
-  public void share(View v) {
+  private void createBottomBar(Bundle savedInstanceState) {
+    mBottomBar = BottomBar.attach(this, savedInstanceState);
+    mBottomBar.noTabletGoodness();
+    mBottomBar.setItemsFromMenu(R.menu.bottombar_menu, new OnMenuTabClickListener() {
+      @Override
+      public void onMenuTabSelected(@IdRes int menuItemId) {
+        if (menuItemId == R.id.bottomBarItemOne) {
+          mDeviceLayout.setVisibility(View.VISIBLE);
+          mOsLayout.setVisibility(View.VISIBLE);
+          mScreenLayout.setVisibility(View.GONE);
+          mOtherLayout.setVisibility(View.GONE);
+        } else if (menuItemId == R.id.bottomBarItemTwo) {
+          mDeviceLayout.setVisibility(View.GONE);
+          mOsLayout.setVisibility(View.GONE);
+          mScreenLayout.setVisibility(View.VISIBLE);
+          mOtherLayout.setVisibility(View.GONE);
+        } else if (menuItemId == R.id.bottomBarItemThree) {
+          mDeviceLayout.setVisibility(View.GONE);
+          mOsLayout.setVisibility(View.GONE);
+          mScreenLayout.setVisibility(View.GONE);
+          mOtherLayout.setVisibility(View.VISIBLE);
+        }
+      }
+
+      @Override
+      public void onMenuTabReSelected(@IdRes int menuItemId) {
+        if (menuItemId == R.id.bottomBarItemOne) {
+          // The user reselected item number one, scroll your content to top.
+        }
+      }
+    });
+  }
+
+  public void share() {
     final StringBuilder sb = new StringBuilder();
     sb.append(getString(R.string.operating_system)).append("\n");
     for (Stat s : osStats) {
@@ -82,6 +142,7 @@ public class SpecsActivity extends Activity {
   }
 
   private void loadStats() {
+    deviceStats = new ArrayList<>();
     osStats = new ArrayList<>();
     screenStats = new ArrayList<>();
     otherStats = new ArrayList<>();
@@ -101,6 +162,18 @@ public class SpecsActivity extends Activity {
       codename = codenames[Math.min(Build.VERSION.SDK_INT - 1, codenames.length - 1)];
     }
     String locale = config.locale.getDisplayName();
+    String manufacturer = Build.MANUFACTURER;
+    String model = Build.MODEL;
+
+    TextView makeView = (TextView) findViewById(R.id.device_make);
+    TextView modelView = (TextView) findViewById(R.id.device_model);
+
+    deviceStats.add(new Stat(R.string.make, manufacturer, makeView));
+    deviceStats.add(new Stat(R.string.model, model, modelView));
+
+    for (Stat s : deviceStats) {
+      s.updateView();
+    }
 
     TextView sdkVerView = (TextView) findViewById(R.id.sdk_ver_text);
     TextView releaseView = (TextView) findViewById(R.id.release_text);
@@ -208,5 +281,40 @@ public class SpecsActivity extends Activity {
   private String getScreenSizeDp(DisplayMetrics dm) {
     return ((int) (dm.widthPixels / dm.density)) + "dp by " +
         ((int) (dm.heightPixels / dm.density)) + "dp";
+  }
+
+  public String getDeviceName() {
+    String manufacturer = Build.MANUFACTURER;
+    String model = Build.MODEL;
+    if (model.startsWith(manufacturer)) {
+      return capitalize(model);
+    } else {
+      return capitalize(manufacturer) + " " + model;
+    }
+  }
+
+
+  private String capitalize(String s) {
+    if (s == null || s.length() == 0) {
+      return "";
+    }
+    char first = s.charAt(0);
+    if (Character.isUpperCase(first)) {
+      return s;
+    } else {
+      return Character.toUpperCase(first) + s.substring(1);
+    }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.main_menu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    share();
+    return super.onOptionsItemSelected(item);
   }
 }
